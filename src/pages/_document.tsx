688c3528ae2090273706d5e292cf4e545a30a132
iownducks <rednetworkapp@gmail.com>
2025-08-31 06:19:23 +0200

    Update _document.tsx: Modify favicon links to include versioning and enhance icon references for improved browser compatibility.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.svg?v=1" />
+        <link rel="shortcut icon" href="/favicon.svg?v=1" />
         <meta name="theme-color" content="#fbbf24" />
       </Head>
       <body className="antialiased">
